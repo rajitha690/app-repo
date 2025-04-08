9eb2be9a927854c30cab78d464822e538b2a3f80
dimpleswapna <swapnasowju2011@gmail.com>
2025-04-08 10:49:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+# app.py
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from Jenkins pipeline!"
+@app.route("/")
+def home():
+    return "Hello from Jenkins + SonarQube pipeline!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/health")
+def health():
+    return {"status": "UP"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
